4d6e6f6b649c0942e1e896ebd91edccfb916d507
carlstep <>
2021-04-23 06:18:16 +0000

    reviewed class constructors, instance methods, inheritance, super-constructor, toString method and overriding toString method.

--- main.dart
+++ main.dart
@@ -1,9 +1,9 @@
+
 void main() {
   
-  //final person = Person(name: 'Andrea', age: 34, height: 1.84);
-  //print(person.describe());
   final employee = Employee(name: 'Andrea', age: 34, height: 1.84, taxCode: 'AB12', salary: 50000);
   employee.sayName();
+  print(employee.toString());
 }
 
 class Person {
@@ -12,6 +12,8 @@ class Person {
   final int age;
   final double height;
   
+  @override
+  String toString() => 'name: $name, age: $age';
   String describe() => 
     'hello, I\'m $name. I\'m $age years old. I\'m $height meters tall';
 
@@ -19,12 +21,15 @@ class Person {
   
 }
 
-// below is the subclass, which 'extends' the class
+// below is the subclass(Employee), which 'extends' the super-class(Person)
 class Employee extends Person {
   Employee({String name, int age, double height, this.taxCode, this.salary}) 
-    : super(name: name, age: age, height: height);
+    : super(name: name, age: age, height: height); // these values are added to the sub-class (employee) 'super-class' - Person
   final String taxCode;
   final int salary;
+  
+  @override
+  String toString() => '${super.toString()}, taxCode: $taxCode, salary: $salary';
 }
 
 // Object class 
